4f45e254f492bb81c7a507f04cc58d6ceb489b83
Dmitry Kuznetsov <kuznetsov.da@phystech.edu>
2026-08-17 15:01:11 +0300

    развернул if else в линейную структуру

--- squaresolver.c
+++ squaresolver.c
@@ -8,13 +8,20 @@ int main(void)
     printf("ax^2+bx+c input a,b,c\n");
     scanf("%f %f %f",&a,&b,&c);
     D = b*b - 4*a*c;
-    if (D<0) {
+
+    /* D < 0 case */
+    if (D < 0) {
         printf("No roots\n");
-    } else {
-        if (D == 0) {
-            printf("x1=%f\n",-b/(2*a));
-        } else {
-            printf("x1=%f\tx2=%f\n",(-b+sqrt(D))/(2*a),(-b-sqrt(D))/(2*a));
-        }
+        return 0;
     }
+
+    /* D == 0 case */
+    if (D == 0) {
+        printf("x1=%f\n",-b/(2*a));
+        return 0;
+    }
+
+    /* D > 0 case */
+    printf("x1=%f\tx2=%f\n",(-b+sqrt(D))/(2*a),(-b-sqrt(D))/(2*a));
+    return 0;
 }
