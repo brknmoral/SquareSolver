46c50cc97c188c1d40a21a077a3a23a68c50bb32
Dmitry Kuznetsov <kuznetsov.da@phystech.edu>
2026-08-17 14:00:37 +0300

    Исправлены ошибки компиляции в файле squarsolver.c

--- squaresolver.c
+++ squaresolver.c
@@ -1,16 +1,20 @@
-# include <stdio.h>
+#include <stdio.h>
 #include <math.h>
-int main(void){
+
+int main(void)
+{
     float a,b,c;
     float D;
-    scanf('%f %f %f',a,b,c)
-    D = b*b - 4*a*c
-    if (D<0){
-        printf("No roots")}
-    else{
-        if (D == 0){
-            printf("%f",-b/(2*a))}
-        else{
-            printf("%f\t%f",(-b+sqrt(D)))/(2*a),(-b-sqrt(D)))/(2*a))}
+    printf("ax^2+bx+c input a,b,c\n");
+    scanf("%f %f %f",&a,&b,&c);
+    D = b*b - 4*a*c;
+    if (D<0) {
+        printf("No roots\n");
+    } else {
+        if (D == 0) {
+            printf("x1=%f\n",-b/(2*a));
+        } else {
+            printf("x1=%f\tx2=%f\n",(-b+sqrt(D))/(2*a),(-b-sqrt(D))/(2*a));
         }
+    }
 }
